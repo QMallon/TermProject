0b9788647fcf44b282fae12d5ce628c293c98260
QMallon <57999351+QMallon@users.noreply.github.com>
2020-04-18 12:33:26 -0400

    fixed one error

---
+++ TermProject/TermProject/ProfileView.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/TermProject.Master" AutoEventWireup="true" CodeBehind="ProfileView.aspx.cs" Inherits="TermProject.WebForm4" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ TermProject/TermProject/ProfileView.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TermProject
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            strUsername = Session["Username"].ToString();
+            if(strUsername == "")
+            {
+                viewProfilenonUser();
+            }
+        }
+    }
+}
